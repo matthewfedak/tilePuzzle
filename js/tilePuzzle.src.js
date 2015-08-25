4849538513b50f9ec2645347362fc3265cfd00e7
Matthew Fedak <matthewfedak@hotmail.com>
2015-08-25 14:37:15 +0100

    CSS and JS formatting tidied via jshint and csslinter
    Have updated CSS and JS to comply with CSSLint and JSHint

--- js/tilePuzzle.src.js
+++ js/tilePuzzle.src.js
@@ -2,7 +2,12 @@
  * tilePuzzle.src.js
  *
  * A simple tile puzzle jQuery plugin by @matthewfedak
+ *
  */
+
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
 (function ($) {
 
     "use strict";
@@ -10,6 +15,8 @@
     $.fn.tilePuzzle = function (options) {
 
         var self = $(this);
+
+        var tilePuzzle = null;
 
         var defaults = {
             keyAccess: false,
@@ -43,7 +50,7 @@
 
                 _this.loadRemoteImage();
 
-                _this.container.on('click', 'li', function(){
+                _this.container.on('click', 'li', function () {
 
                     _this.moveTile($(this), $(this).css('top'), $(this).css('left'));
 
@@ -59,13 +66,13 @@
 
                 var img = new Image();
 
-                img.onload = function(){
+                img.onload = function () {
 
                     _this.imageDimensions = { 'width': this.width, 'height': this.height };
 
                     if (_this.imageDimensions.width > _this.maxWidth) {
 
-                        var newHeight = (_this.maxWidth/_this.imageDimensions.width)*this.height;
+                        var newHeight = (_this.maxWidth / _this.imageDimensions.width) * this.height;
                         _this.imageDimensions.width = _this.maxWidth;
                         _this.imageDimensions.height = newHeight;
 
@@ -84,29 +91,30 @@
             createTiles : function () {
 
                 var _this = this;
+                var x, n, t = null;
 
                 _this.tileDimensions = {
-                    'width': Math.abs(_this.imageDimensions.width/tilePuzzle.level),
-                    'height': Math.abs(_this.imageDimensions.height/tilePuzzle.level)
+                    'width': Math.abs(_this.imageDimensions.width / tilePuzzle.level),
+                    'height': Math.abs(_this.imageDimensions.height / tilePuzzle.level)
                 };
 
-                for(var x = 0, n = 0, t = 0;t<tilePuzzle.level*tilePuzzle.level;t++){
+                for (x = 0, n = 0, t = 0; t < tilePuzzle.level * tilePuzzle.level; t++) {
 
                     _this.container.append(
                         $('<li/>').css({
                             'width': _this.tileDimensions.width,
                             'height': _this.tileDimensions.height,
-                            'background-image': 'url('+tilePuzzle.imageUrl+')',
-                            'background-position':'-'+(t*this.tileDimensions.width)+'px -'+(x*_this.tileDimensions.height)+'px',
-                            'background-size':_this.imageDimensions.width+'px '+_this.imageDimensions.height+'px',
-                            'top':(x*_this.tileDimensions.height)+'px',
-                            'left':(n*_this.tileDimensions.width)+'px'
-                            })
+                            'background-image': 'url(' + tilePuzzle.imageUrl + ')',
+                            'background-position': '-' + (t * this.tileDimensions.width) + 'px -' + (x * _this.tileDimensions.height) + 'px',
+                            'background-size': _this.imageDimensions.width + 'px ' + _this.imageDimensions.height + 'px',
+                            'top': (x * _this.tileDimensions.height) + 'px',
+                            'left': (n * _this.tileDimensions.width) + 'px'
+                        })
                     );
 
                     n++;
 
-                    if (((t+1) % tilePuzzle.level) === 0) {
+                    if (((t + 1) % tilePuzzle.level) === 0) {
                         x++;
                         n = 0;
                     }
@@ -136,18 +144,18 @@
 
                 _this = this;
 
-                limit = limit*limit;
+                limit = limit * limit;
 
                 i = 0;
 
-                setTimeout(function(){
-                    timer = setInterval(function(){
+                setTimeout(function () {
+                    timer = setInterval(function () {
 
-                        tile1 = _this.container.children().eq(Math.floor((Math.random()*(tilePuzzle.level*tilePuzzle.level))+1));
+                        tile1 = _this.container.children().eq(Math.floor((Math.random() * (tilePuzzle.level * tilePuzzle.level)) + 1));
 
-                        tile2 = _this.container.children().eq(Math.floor((Math.random()*(tilePuzzle.level*tilePuzzle.level))+1));
+                        tile2 = _this.container.children().eq(Math.floor((Math.random() * (tilePuzzle.level * tilePuzzle.level)) + 1));
 
-                        tile1Position = { 'top':tile1.css('top'), 'left':tile1.css('left') };
+                        tile1Position = { 'top': tile1.css('top'), 'left': tile1.css('left') };
 
                         tile1.css({
                             'top': tile2.css('top'),
@@ -159,7 +167,7 @@
                             'left': tile1Position.left
                         });
                         i++;
-                        if(i>limit){
+                        if (i > limit) {
                             clearInterval(timer);
                         }
                     }, 50);
@@ -169,35 +177,35 @@
 
             moveTile: function (tile, top, left) {
 
-                var _this, topDistance, leftDistance, newSparePosition;
+                var _this, topDistance, leftDistance;
 
                 _this = this;
 
-                topDistance = Math.abs(parseInt(top)-parseInt(_this.spareTile.css('top')));
+                topDistance = Math.abs(parseInt(top, 10) - parseInt(_this.spareTile.css('top'), 10));
 
-                leftDistance = Math.abs(parseInt(left)-parseInt(_this.spareTile.css('left')));
+                leftDistance = Math.abs(parseInt(left, 10) - parseInt(_this.spareTile.css('left'), 10));
 
-                if((leftDistance+topDistance) <= Math.round(_this.tileDimensions.width) || (leftDistance+topDistance) <= Math.round(_this.tileDimensions.height)){
+                if ((leftDistance + topDistance) <= Math.round(_this.tileDimensions.width) || (leftDistance + topDistance) <= Math.round(_this.tileDimensions.height)) {
 
                     tile.css({
-                        'top':_this.spareTile.css('top'),
-                        'left':_this.spareTile.css('left')
+                        'top': _this.spareTile.css('top'),
+                        'left': _this.spareTile.css('left')
                     });
 
                     _this.spareTile.css({
-                        'left':left,
-                        'top':top
+                        'left': left,
+                        'top': top
                     });
 
                 }
             }
         };
 
-        var tilePuzzle = $.extend(construct, defaults, options);
+        tilePuzzle = $.extend(construct, defaults, options);
 
         tilePuzzle.init();
 
         return self;
     };
 
-})( jQuery );
+}(jQuery));
